2b83fc7d3fa75ef968f2edfb35d6191c49afedc7
kalai02484 <kalai02484@gmail.com>
2026-03-07 03:14:43 +0530

    middleware completed

---
+++ middleware/middleware.js
@@ -0,0 +1,48 @@
+import jwt from "jsonwebtoken";
+import User from "../models/userSchema.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const authMiddleware = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        message: "Token Missing",
+      });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    console.log("Decoded Token:", decoded);
+
+    const user = await User.findById(decoded._id).select("-password");
+
+    if (!user) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid Token",
+      error: error.message,
+    });
+  }
+};
+
+export const adminMiddleware = async (req, res, next) => {
+  if (req.user.role === "admin") {
+    next();
+  } else {
+    return res.status(403).json({
+      message: "Access Denied",
+    });
+  } 
+}
